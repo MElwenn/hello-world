fb5fa88c937563bb1e5f51b22480c9a7d0cf3a12
MElwenn <63125829+MElwenn@users.noreply.github.com>
2021-01-10 11:36:35 +0100

    installed firebase, updated chat.js accordingly

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Alert, Button, ScrollView } from 'react-native';
+//import { StyleSheet, Text, View, TextInput, Alert, Button, ScrollView } from 'react-native'; // non of it is actually used (anymore)
 
 // import the screens
 import Start from './components/Start';
@@ -12,12 +12,38 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// import firebase
+//const firebase = require('firebase');
+//require('firebase/firestore');
+
 // Create the navigator
 const Stack = createStackNavigator();
+
+// Create firebast constructor (remove later)
+/*const firebaseConfig = {
+  apiKey: "AIzaSyCiSiaZpYcUAYUy3UJuCGtscXEFxPZ1WMM",
+  authDomain: "test-4e483.firebaseapp.com",
+  projectId: "test-4e483",
+  storageBucket: "test-4e483.appspot.com",
+  messagingSenderId: "920129606551",
+  appId: "1:920129606551:web:23a395bc1420f247a36ad2"
+};*/
+
+/*if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+this.referenceShoppingLists = firebase.firestore().collection('shoppinglists');*/
 
 export default class App extends Component {
 
   render() {
+
+    // remove later (as needed for the shopping list example only)
+    //this.state = {
+    //  lists: [],
+    //};
+
     return (
       <NavigationContainer>
         <Stack.Navigator
--- components/Chat.js
+++ components/Chat.js
@@ -1,52 +1,146 @@
 import React from 'react';
 import { KeyboardAvoidingView, Platform, View, StyleSheet } from 'react-native';
-import { GiftedChat, Bubble, SystemMessage } from 'react-native-gifted-chat';
-
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 /* ERROR: index.js:1 Warning: Cannot update a component (`StackNavigator`) 
 while rendering a different component (`Chat`). To locate the bad setState() 
 call inside `Chat`, follow the stack trace as described in https://fb.me/setstate-in-render */
 
+// import Firestore
+const firebase = require('firebase');
+require('firebase/firestore');
+
 /* Create the chat component */
 export class Chat extends React.Component {
     constructor() {
         super();
+
         this.state = {
             messages: [],
+            user: {
+                _id: '',
+                name: '',
+                avatar: ''
+            },
+            uid: 0,  // is this really needed if I have ONE chatroom for all users anywhay?
+            loggedInText: '',
+            isConnected: false,
+            image: '',
+            location: ''
         }
+
+        // allow app to connect to Firestore
+        const firebaseConfig = {
+            apiKey: "AIzaSyD2D9ZsvijtkGdVuO3VPe2NxJzVzflJhPA",
+            authDomain: "chatapp-23e15.firebaseapp.com",
+            databaseURL: "https://chatapp-23e15.firebaseapp.com", // Is this needed?
+            projectId: "chatapp-23e15",
+            storageBucket: "chatapp-23e15.appspot.com",
+            messagingSenderId: "210840460009",
+            appId: "1:210840460009:web:224052ea77e9e83bd62e78"
+        }
+
+        // initialize Firestore app
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+
+        // reference to Firestore collection
+        this.referenceMessages = firebase.firestore().collection('messages');
     }
 
-    /* Exercise 5.3, Task 4.: "add two static messages (a system message and a normal message)" */
     componentDidMount() {
-        this.setState({
-            messages: [
-                {
-                    _id: 1,
-                    text: 'Hello developer',
-                    createdAt: new Date(),
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: 'https://placeimg.com/140/140/any',
-                    },
+
+        // authentication anonymos in firebase
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+            if (!user) {
+                await firebase.auth().signInAnonymously();
+            }
+
+            //update user state with currently active user data
+            this.setState({
+                isConnected: true,
+                uid: user.uid,
+                user: {
+                    _id: user.uid,
+                    name: this.props.route.params.name,
+                    avatar: 'https://placeimg.com/140/140/any'
                 },
-                {
-                    _id: 2,
-                    text: 'Hello, you have entered the chat',
-                    createdAt: new Date(),
-                    system: true,
+                //loggedInText: 'Hello there',
+                loggedInText: `${this.props.route.params.name} wrote`,
+                messages: []
+            });
+
+            /*update user state with currently active user data
+            this.setState({
+                user: {
+                    _id: user._id,
+                    name: this.props.route.params.user,
                 },
-            ],
-        })
+                messages: [],
+            });*/
+        });
+
+        this.referenceMessages = firebase.firestore().collection('messages');
+
+        // display messages in chronological order & onSnapshot observer
+        this.unsubscribe = this.referenceMessages.orderBy('createdAt', 'desc').onSnapshot(this.onCollectionUpdate);
+        //this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate) // ERROR: [react-native-gifted-chat] GiftedChat: `_id` is missing for message {"image":"","location":""}
     }
 
-    //componentWillUnmount() { }
+    componentWillUnmount() {
+        this.unsubscribe();
+        this.authUnsubscribe();
+    }
 
     /* function called as soon as user types a message */
     onSend(messages = []) {
         this.setState(previousState => ({
             messages: GiftedChat.append(previousState.messages, messages),
-        }))
+        }), () => {
+            this.addMessage();
+        })
+    }
+
+    onCollectionUpdate = (querySnapshot) => {
+        const messages = [];
+        // go through each document
+        querySnapshot.forEach((doc) => {
+            // get the QueryDocumentSnapshot's data
+            var data = doc.data();
+            messages.push({
+                _id: data._id,
+                text: data.text,
+                //text: data.text.toString(), // ERROR (console): Chat.js:116 Uncaught TypeError: Cannot read property 'toDate' of undefined 
+                createdAt: data.createdAt, // ERROR (browser): invalid date 
+                //createdAt: data.createdAt.toDate(),
+                //user: data.user,
+                user: {
+                    _id: data.user._id,
+                    name: data.user.name,
+                    avatar: data.user.avatar
+                },
+                image: data.image || '',
+                location: data.location || ''
+            });
+        });
+        this.setState({
+            messages,
+        });
+    };
+
+    // add message object to firestore-collection
+    addMessage() {
+        const message = this.state.messages[0];
+        this.referenceMessages.add({
+            _id: message._id,
+            text: message.text || '',
+            createdAt: message.createdAt,
+            user: message.user,
+            uid: this.state.uid,
+            image: message.image || '',
+            location: message.location || ''
+        });
     }
 
     render() {
@@ -70,9 +164,7 @@ export class Chat extends React.Component {
                     renderBubble={this.renderBubble.bind(this)}
                     messages={this.state.messages}
                     onSend={messages => this.onSend(messages)}
-                    user={{
-                        _id: 1,
-                    }}
+                    user={this.state.user}
                 />
                 {/* avoid android keybords to overlay messages */}
                 {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
